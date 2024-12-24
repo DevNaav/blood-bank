58b3c9b8e67716fa930ef0496289b0d4e2fc04f8
parashjain2810 <parashjain2810@gmail.com>
2024-12-24 17:37:06 +0530

    Resolved

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express =require('express');
 const app = express();
 const database = require("./config/database");
 const userRoutes = require("./routes/User");
-
+const cors=require("cors");
 
 
 database.connect();
 app.use(express.json());
+app.use(cors({
+  origin:"*",
+  methods:["GET","PUT",'DELETE',"UPDATE","POST"],
+  credentials:true
+}));
 app.use("/api/", userRoutes);
 app.get('/',(req,res)=>{
   res.send("Sbh  bhdia hai");
